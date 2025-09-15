2ed2d07db86393a8714e731d4173622750071fb8
MRafliRamadhani <mrafliramadhani21@gmail.com>
2025-09-16 03:58:04 +0700

    scheduler

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule untuk safety stock calculation - jalan setiap hari jam 2 pagi
+Schedule::command('safety-stock:calculate')->dailyAt('02:00');
